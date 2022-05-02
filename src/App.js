e6aa6038e6ff1233467908eadf53cca076ae8e98
gamersindo1223 <76719536+gamersindo1223@users.noreply.github.com>
2022-05-02 20:28:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header'
 import Card from './components/Card'
 import Footer from './components/Footer'
 import items from './assets/items'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './css/skeleton.css'
 import './css/normalize.css'
