1c8503a29dfa9540160438732b4d2585f5467082
gamersindo1223 <76719536+gamersindo1223@users.noreply.github.com>
2022-05-03 19:50:44 +0800

    e

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NotFound from './NotFound';
 const Web = () => (
-    <Router>
-    <Routes>
+    < React.StrictMode >
+     <Router>
+      <Routes>
       <Route exact path="/" component={<App/>} />
       <Route component={<NotFound/>} />
-    </Routes>
-  </Router> 
+     </Routes>
+     </Router> 
+    </React.StrictMode>
+
 )
 //ReactDOM.render( < React.StrictMode ></React.StrictMode>,document.getElementById('root'));
 render(<Web/>, document.getElementById('root'));
