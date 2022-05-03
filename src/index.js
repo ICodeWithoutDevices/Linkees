df8c5d71477763e558722556d4f9801fe019370b
gamersindo1223 <76719536+gamersindo1223@users.noreply.github.com>
2022-05-03 19:23:15 +0800

    f

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NotFound from './NotFound';
 
 ReactDOM.render( < React.StrictMode >
-    <App />
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
     </React.StrictMode>,
     document.getElementById('root')
 );
