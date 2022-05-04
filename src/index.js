a20964549a679adb8040ccff0b4416d9c6c75b40
gamersindo1223 <76719536+gamersindo1223@users.noreply.github.com>
2022-05-04 16:52:00 +0800

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NotFound from './NotFound';
 const Web = () => (
